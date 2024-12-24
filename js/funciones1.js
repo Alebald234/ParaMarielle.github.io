90ee5e0171542df9ee9741948bd6afcc969b104b
Alebald234 <153924127+Alebald234@users.noreply.github.com>
2024-12-24 23:51:20 +0100

    Add files via upload

---
+++ js/funciones1.js
@@ -0,0 +1,133 @@
+/*function lasfotos(){
+
+    document.getElementById("foto2").style.display = 'block';
+
+    document.getElementById("foto3").style.display = 'block';
+
+    document.getElementById("botonmostrar").style.display = 'none';
+
+    document.getElementById("botonesconder").style.display = 'block';
+
+}
+
+function ocultar(){
+
+    document.getElementById("foto2").style.display = 'none';
+
+    document.getElementById("foto3").style.display = 'none';
+
+    document.getElementById("botonmostrar").style.display = 'block';
+
+    document.getElementById("botonesconder").style.display = 'none';
+}
+
+function princesa(){
+
+    document.getElementById("foto2").style.display = 'none';
+
+    document.getElementById("princesa").style.display = 'block';
+
+    document.getElementById("botonmostrar").style.display = 'block';
+
+    document.getElementById("botonesconder").style.display = 'none';
+
+}
+
+function mostrarfotos(){
+
+    document.getElementById("foto2").style.display = 'none';
+
+    document.getElementById("foto3").style.display = 'none';
+
+    document.getElementById("botonmostrar").style.display = 'block';
+
+    document.getElementById("botonesconder").style.display = 'none';
+}
+    */
+
+
+function mostrarprimavera(){
+    document.getElementById("primavera").style.display="block";
+    document.getElementById("audprim").style.display="block";
+    document.getElementById("canciones").style.display="none";
+    document.getElementById("songs").style.display="none";
+}
+
+function mostrarreloj(){
+    document.getElementById("reloj").style.display="block";
+    document.getElementById("audreloj").style.display="block";
+    document.getElementById("canciones").style.display="none";
+    document.getElementById("songs").style.display="none";
+}
+
+function ocultarventana(){
+    document.getElementById("vent").style.display="none";
+}
+
+function mostrarcarta1(){
+    document.getElementById("carta1").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta2(){
+    document.getElementById("carta2").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta3(){
+    document.getElementById("carta3").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta4(){
+    document.getElementById("carta4").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta5(){
+    document.getElementById("carta5").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta6(){
+    document.getElementById("carta6").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta7(){
+    document.getElementById("carta7").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta8(){
+    document.getElementById("carta8").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta9(){
+    document.getElementById("carta9").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta10(){
+    document.getElementById("carta10").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+function mostrarcarta11(){
+    document.getElementById("carta11").style.display="block";
+    document.getElementById("cartas").style.display="none";
+    document.getElementById("cartamensaje").style.display="none";
+}
+
+
